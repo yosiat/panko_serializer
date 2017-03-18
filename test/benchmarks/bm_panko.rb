09415d6caeea138bcbd3d68b54a522c9757ac81f
Yosi Attias <yosy101@gmail.com>
2017-03-18 12:48:36 +0200

    Adapting panic benchmarks to the new array serializer interface.

--- test/benchmarks/bm_panko.rb
+++ test/benchmarks/bm_panko.rb
@@ -29,24 +29,22 @@ def benchmark prefix, serializer
   posts_50 = posts.first(50).to_a
 
 
-  Benchmark.ams("Panko_#{prefix}_Posts_#{posts.count}") do
-   Panko::ArraySerializer.new(posts, each_serializer: serializer).serializable_object
-  end
+	Benchmark.ams("Panko_#{prefix}_Posts_#{posts.count}") do
+	 Panko::ArraySerializer.new(posts, each_serializer: serializer).serializable_object
+	end
 
-  Benchmark.ams("Panko_#{prefix}_Posts_50") do
-   Panko::ArraySerializer.new(posts_50, each_serializer: serializer).serializable_object
-  end
+	Benchmark.ams("Panko_#{prefix}_Posts_50") do
+	 Panko::ArraySerializer.new(posts_50, each_serializer: serializer).serializable_object
+	end
 
   posts_array_serializer = Panko::ArraySerializer.new(posts, each_serializer: serializer)
 
   Benchmark.ams("Panko_Reused_#{prefix}_Posts_#{posts.count}") do
-    posts_array_serializer.subjects = posts
-    posts_array_serializer.serializable_object
+    posts_array_serializer.serialize posts
   end
 
   Benchmark.ams("Panko_Reused_#{prefix}_Posts_50") do
-    posts_array_serializer.subjects = posts_50
-    posts_array_serializer.serializable_object
+    posts_array_serializer.serialize posts_50
   end
 end
 
