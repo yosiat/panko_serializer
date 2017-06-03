1bb57f30bd92456bb9f8a493a986513bf5cede5b
Yosi Attias <yosy101@gmail.com>
2017-06-03 16:47:28 +0300

    Use ObjectWriter in serializer

--- lib/panko/object_writer.rb
+++ lib/panko/object_writer.rb
@@ -1,15 +1,18 @@
-
 module Panko
   class ObjectWriter
     attr_reader :output
 
     def initiailize
       @output = nil
+
       @current_pushed_type = nil
+      @last_pushed_type = nil
     end
 
     def push_array
       @output = [] if @output.nil?
+
+      @last_pushed_type = @current_pushed_type
       @current_pushed_type = :array
     end
 
@@ -17,6 +20,12 @@ module Panko
       @output = {} if @output.nil?
       @output << {} if @output.is_a? Array
 
+      if @current_pushed_type == :key
+        pushed_key = @output.keys.last
+        @output[pushed_key] = {}
+      end
+
+      @last_pushed_type = @current_pushed_type
       @current_pushed_type = :object
     end
 
@@ -24,11 +33,37 @@ module Panko
       if @current_pushed_type == :object
         cursor = @output if @output.is_a? Hash
         cursor = @output.last if @output.is_a? Array
-        cursor[key] = value
+
+        if @last_pushed_type == :key
+          pushed_key = cursor.keys.last
+          cursor[pushed_key][key] = value
+        else
+          cursor[key] = value
+        end
+      end
+
+      if @current_pushed_type == :key
+        cursor = @output if @output.is_a? Hash
+        cursor = @output.last if @output.is_a? Array
+
+        pushed_key = cursor.keys.last
+        cursor[pushed_key] = value
       end
 
       if @current_pushed_type == :array
         @output << value
+      end
+    end
+
+    def push_key(key)
+      if @current_pushed_type == :object
+        cursor = @output if @output.is_a? Hash
+        cursor = @output.last if @output.is_a? Array
+
+        @last_pushed_type = @current_pushed_type
+        @current_pushed_type = :key
+
+        cursor[key] = nil
       end
     end
 
