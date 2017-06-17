576bcab6d20bc02b5da8c9b1120c1459808462a8
Yosi Attias <yosy101@gmail.com>
2017-06-17 11:59:15 +0300

    Fix controllers benchmark

--- benchmarks/app.rb
+++ benchmarks/app.rb
@@ -1,8 +1,12 @@
 require 'bundler/setup'
 
+
+require 'rails'
 require 'active_model'
 require 'active_support'
 require 'active_support/json'
+require 'action_controller'
+require 'action_controller/railtie'
 
 require 'active_model_serializers'
 require 'panko'
