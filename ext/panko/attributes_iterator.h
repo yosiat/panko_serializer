8a5612ad7ccc76e2be3d80414dd962750ad43feb
Yosi Attias <yosy101@gmail.com>
2017-07-08 15:55:20 +0300

    Extracting attributes iteration to it’s own file

---
+++ ext/panko/attributes_iterator.h
@@ -0,0 +1,5 @@
+#include <ruby.h>
+
+typedef void  (*EachAttributeFunc)(VALUE object, VALUE name, VALUE value, VALUE type_metadata, VALUE context);
+
+extern VALUE panko_each_attribute(VALUE object, VALUE attributes, EachAttributeFunc func, VALUE context);
--- ext/panko/extconf.rb
+++ ext/panko/extconf.rb
@@ -1,5 +1,8 @@
 require 'mkmf'
 
+$CPPFLAGS += " -Wall"
+
 extension_name = 'panko'
 dir_config(extension_name)
 create_makefile(extension_name)
+%x{make clean}
