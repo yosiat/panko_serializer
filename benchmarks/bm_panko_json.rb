88966b5331a8bc834b49b9bd2a02a4bbba7febd9
Yosi Attias <yosy101@gmail.com>
2017-07-19 00:03:18 +0300

    Removed reused benchmarks and add new benchmark

--- benchmarks/bm_panko_json.rb
+++ benchmarks/bm_panko_json.rb
@@ -11,6 +11,14 @@ class PostFastSerializer < Panko::Serializer
   attributes :id, :body, :title, :author_id
 end
 
+class PostFastWithMethodCallSerializer < Panko::Serializer
+  attributes :id, :body, :title, :author_id, :method_call
+
+  def method_call
+    object.id * 2
+  end
+end
+
 class PostWithHasOneFastSerializer < Panko::Serializer
   attributes :id, :body, :title, :author_id
 
@@ -42,27 +50,10 @@ def benchmark(prefix, serializer, options = {})
   Benchmark.ams("Panko_#{prefix}_Posts_50") do
     Panko::ArraySerializer.new(posts_50, merged_options).to_json
   end
-
-  posts_array_serializer = Panko::ArraySerializer.new([], merged_options)
-
-  data = Benchmark.data
-  posts = data[:all]
-  posts_50 = data[:small]
-
-  Benchmark.ams("Panko_Reused_#{prefix}_Posts_#{posts.count}") do
-    posts_array_serializer.serialize_to_json posts
-  end
-
-  data = Benchmark.data
-  posts = data[:all]
-  posts_50 = data[:small]
-
-  Benchmark.ams("Panko_Reused_#{prefix}_Posts_50") do
-    posts_array_serializer.serialize_to_json posts_50
-  end
 end
 
 benchmark 'HasOne', PostWithHasOneFastSerializer
 benchmark 'Simple', PostFastSerializer
+benchmark 'SimpleWithMethodCall', PostFastWithMethodCallSerializer
 benchmark 'Except', PostWithHasOneFastSerializer, except: [:title]
 benchmark 'Include', PostWithHasOneFastSerializer, include: [:id, :body, :author_id, :author]
