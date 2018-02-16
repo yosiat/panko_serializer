1460432c103457cd93211e8e9966ca437c82c712
Yosi Attias <yosy101@gmail.com>
2018-02-16 22:23:41 +0200

    support nesting in response

--- lib/panko/response.rb
+++ lib/panko/response.rb
@@ -19,18 +19,36 @@ module Panko
       writer.push_object
 
       @data.each do |key, value|
-        if value.is_a?(Panko::ArraySerializer) || value.is_a?(Panko::Serializer)
-          writer.push_json(value.to_json, key.to_s)
-        elsif value.is_a?(Panko::JsonValue)
-          writer.push_json(value.value, key.to_s)
-        else
-          writer.push_value(value, key.to_s)
+        key = key.to_s
+
+        if value.is_a? Array
+          writer.push_array key
+          value.each { |v| write_object(writer, v) }
+          writer.pop
+          next
         end
+
+        write_object writer, value, key
+
       end
 
       writer.pop
 
       writer.to_s
+    end
+
+    private
+
+    def write_object(writer, value, key = nil)
+     if value.is_a?(Panko::ArraySerializer) ||
+        value.is_a?(Panko::Serializer) ||
+        value.is_a?(Panko::Response)
+       writer.push_json(value.to_json, key)
+     elsif value.is_a?(Panko::JsonValue)
+       writer.push_json(value.value, key)
+     else
+       writer.push_value(value, key)
+     end
     end
   end
 end
