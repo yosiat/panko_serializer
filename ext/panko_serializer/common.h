adcc76438b052600ba2309c49a2c81b47e7535fc
Yosi Attias <yosy101@gmail.com>
2019-06-01 17:03:40 +0300

    Read from delegate hash only if it’s bigger then “values” hash
    Do reduce false positives

--- ext/panko_serializer/common.h
+++ ext/panko_serializer/common.h
@@ -3,6 +3,9 @@
 #include <ruby.h>
 
 
+#define PANKO_SAFE_HASH_SIZE(hash) \
+  (hash == Qnil || hash == Qundef) ? 0 : RHASH_SIZE(hash)
+
 #define PANKO_EMPTY_HASH(hash) \
   (hash == Qnil || hash == Qundef) ? 1 : (RHASH_SIZE(hash) == 0)
 
