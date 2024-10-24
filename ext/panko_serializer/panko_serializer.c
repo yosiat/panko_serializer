5df5a0814b5ab92ca39975a4b8e08c93ae3d126e
Yosi Attias <yosy101@gmail.com>
2024-10-24 12:37:46 +0000

    ext: cleanup type_cast

--- ext/panko_serializer/panko_serializer.c
+++ ext/panko_serializer/panko_serializer.c
@@ -1,9 +1,22 @@
-#include "panko_serializer.h"
-
 #include <ruby.h>
+
+#include "time_conversion.h"
+
+VALUE public_is_iso8601_time_string(VALUE klass, VALUE value) {
+  return is_iso8601_time_string(StringValuePtr(value)) ? Qtrue : Qfalse;
+}
+
+VALUE public_iso_ar_iso_datetime_string(VALUE klass, VALUE value) {
+  return iso_ar_iso_datetime_string(StringValuePtr(value));
+}
 
 void Init_panko_serializer() {
   VALUE mPanko = rb_define_module("Panko");
 
-  panko_init_type_cast(mPanko);
+  rb_define_singleton_method(mPanko, "is_iso8601_time_string",
+                             public_is_iso8601_time_string, 1);
+  rb_define_singleton_method(mPanko, "iso_ar_iso_datetime_string",
+                             public_iso_ar_iso_datetime_string, 1);
+
+  panko_init_time(mPanko);
 }
--- ext/panko_serializer/panko_serializer.h
+++
@@ -1,3 +0,0 @@
-#include <ruby.h>
-
-#include "attributes_writer/type_cast/type_cast.h"
