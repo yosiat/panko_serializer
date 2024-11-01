a2daa13c9a1d34742ff41b68ec4a8cd76e273b2f
Yosi Attias <yosy101@gmail.com>
2024-11-01 21:21:12 +0000

    attributes_writer/active_record: perf improvement
    * Reduce calls of `instance_variable_get`
    * Simplfiy iteration

--- lib/panko/impl/attributes_writer/active_record/writer.rb
+++ lib/panko/impl/attributes_writer/active_record/writer.rb
@@ -9,12 +9,18 @@ module Panko::Impl::AttributesWriter::ActiveRecord
       attributes_ctx = Context.new(object)
       object_class = object.class
 
-      descriptor.attributes.each do |attribute|
+      length = descriptor.attributes.length
+      i = 0
+      while i < length
+        attribute = descriptor.attributes[i]
+
         attribute.invalidate!(object_class)
 
         value = attributes_ctx.read_attribute(attribute)
 
         ValuesWriter.write(writer, attribute, value)
+
+        i += 1
       end
     end
   end
